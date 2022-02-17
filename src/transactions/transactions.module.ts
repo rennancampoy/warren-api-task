ccda005f079555aba60d6e37a45ca2240f29ffcd
Rennan Campoy <rennan.campoy@connectcom.com.br>
2022-02-16 23:37:30 -0300

    failed attempts of testing and little fixes

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -1,8 +1,8 @@
 import { PortfoliosService } from './../portfolios/portfolios.service';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { transactionSchema } from 'src/models/transaction.model';
-import { customerSchema } from 'src/models/customer.model';
+import { transactionSchema } from './../models/transaction.model';
+import { customerSchema } from './../models/customer.model';
 import { TransactionsController } from './transactions.controller';
 import { TransactionsService } from './transactions.service';
 
